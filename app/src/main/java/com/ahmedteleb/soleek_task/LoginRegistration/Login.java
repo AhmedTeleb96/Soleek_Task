d6ab7133afb3aa805b3f75f4763b874d5c53a296
Ahmed Teleb <ahmedasd37@gmail.com>
2019-05-28 15:34:49 +0200

    Initial commit

---
+++ app/src/main/java/com/ahmedteleb/soleek_task/LoginRegistration/Login.java
@@ -0,0 +1,15 @@
+package com.ahmedteleb.soleek_task.LoginRegistration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ahmedteleb.soleek_task.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
